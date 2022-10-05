1d4d3920216ace061ae2cb882e72ed0ee4423a80
piotrpoleszak <poleszak@protonmail.com>
2022-10-05 23:23:13 +0200

    Initial commit

---
+++ src/main/java/com/poleszak/CostCalculatorApi/CostCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.poleszak.CostCalculatorApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostCalculatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poleszak/CostCalculatorApi/CostCalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poleszak.CostCalculatorApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostCalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
